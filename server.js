d947eeb1d8a381d88c2cd755cf42abe5c2c85365
prudvirajkvs <30479404+prudvirajkvs@users.noreply.github.com>
2021-02-18 13:55:02 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,15 +5,25 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  next();
+});
 async function getInitialBal(user_id) {
   const result = await pool.query(
     'select balance from personalwallet where user_id=$1',
     [user_id]
   );
-
+  console.log(result.rows[0].balance, 'balance');
   return result.rows[0].balance;
 }
-app.get('/', async (req, res) => {
+app.get('/allWallets', async (req, res) => {
   let result = {};
   result = await pool.query('select * from personalwallet');
 
@@ -38,33 +48,36 @@ app.get('/transactions', async (req, res) => {
 });
 app.put('/addFunds', async (req, res) => {
   const { user_id, amount } = req.body;
-
-  const initial_balance = getInitialBal(user_id);
+  // console.log(user_id, amount, 'user id');
+  const initial_balance = await getInitialBal(user_id);
+  console.log(user_id, typeof amount, initial_balance, 'user id');
   await pool
     .query('update personalwallet set balance=$1 where user_id=$2', [
-      initial_balance + amount,
+      (initial_balance + parseFloat(amount)).toFixed(2),
       user_id,
     ])
     .then(async () => {
-      await pool.query(
-        'INSERT INTO public.transactions(user_id, transaction_type, trans_date, initial_balance, final_balance, remarks)	VALUES ($1, $2, $3, $4, $5, $6)',
-        [
-          user_id,
-          'add_funds',
-          Date.now(),
-          initial_balance,
-          initial_balance + amount,
-          'added funds',
-        ]
-      );
+      await pool
+        .query(
+          'INSERT INTO public.transactions(user_id, transaction_type, trans_date, initial_balance, final_balance, remarks)	VALUES ($1, $2, $3, $4, $5, $6)',
+          [
+            user_id,
+            'add_funds',
+            new Date(),
+            initial_balance,
+            (initial_balance + parseFloat(amount)).toFixed(2),
+            'added funds',
+          ]
+        )
+        .then((dt) => res.status(200).send(dt));
     });
 });
 app.put('/spendFunds', async (req, res) => {
-  const { user_id, amount } = pool.query;
-  const initial_balance = getInitialBal(user_id);
+  const { user_id, amount } = req.body;
+  const initial_balance = await getInitialBal(user_id);
   await pool
     .query('update personalwallet set balance=$1 where user_id=$2', [
-      initial_balance - amount,
+      (initial_balance - parseFloat(amount)).toFixed(2),
       user_id,
     ])
     .then(async () => {
@@ -75,7 +88,7 @@ app.put('/spendFunds', async (req, res) => {
           'spend_funds',
           Date.now(),
           initial_balance,
-          initial_balance - amount,
+          (initial_balance - parseFloat(amount)).toFixed(2),
           'deducted funds',
         ]
       );
@@ -89,8 +102,7 @@ app.post('/user', async (req, res) => {
       'INSERT INTO public.personalwallet( username, phone, balance)	VALUES ( $1, $2, $3)',
       [username, phone, balance]
     )
-    .then(() => console.log('query done'))
+    .then((dt) => res.status(200).send(dt))
     .catch((e) => res.status(500).send(e.message));
-  res.status(200).send(result);
 });
 app.listen(PORT, () => console.log(PORT, 'connected'));
